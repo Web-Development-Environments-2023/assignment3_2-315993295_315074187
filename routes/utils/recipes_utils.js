db29b3227293f7fb48e2f9fd4a9960f0b0b2d78e
NirKors <103315566+NirKors@users.noreply.github.com>
2023-05-29 20:43:22 +0300

    Work on `search` function started.

--- routes/utils/recipes_utils.js
+++ routes/utils/recipes_utils.js
@@ -1,7 +1,11 @@
 const axios = require("axios");
 const api_domain = "https://api.spoonacular.com/recipes";
-
-
+const cuisine = ["African", "Asian", "American", "British", "Cajun", "Caribbean", "Chinese", "Eastern European", "European", 
+"French", "German", "Greek", "Indian", "Irish", "Italian", "Japanese", "Jewish", "Korean", "Latin American", "Mediterranean", 
+"Mexican", "Middle Eastern", "Nordic", "Southern", "Spanish", "Thai", "Vietnamese"]
+const diet = ["Gluten Free", "Ketogenic", "Vegetarian", "Lacto-Vegetarian", "Ovo-Vegetarian", "Vegan", "Pescetarian", "Paleo", 
+    "Primal", "Low FODMAP", "Whole30"]
+const intolerance = ["Dairy", "Egg", "Gluten", "Grain", "Peanut", "Seafood", "Sesame", "Shellfish", "Soy", "Sulfite", "Tree Nut", "Wheat"]
 
 /**
  * Get recipes list from spooncular response and extract the relevant recipe data for preview
@@ -38,7 +42,20 @@ async function getRecipeDetails(recipe_id) {
 }
 
 
+async function search_recipes(text, num_of_results, filter1, filter2){
+    // ...
+    await axios.get(`${api_domain}/recipes/complexSearch`, {
+        params: {
+            includeNutrition: false,
+            apiKey: process.env.spooncular_apiKey
+        }
+    });
 
+
+}
+
+
+exports.search_recipes = search_recipes;
 exports.getRecipeDetails = getRecipeDetails;
 
 
